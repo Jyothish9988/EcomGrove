6aee4cb23e3cbbf42997213434e3975a6c6908a0
Jyothish9988 <93032610+Jyothish9988@users.noreply.github.com>
2024-07-07 10:21:58 +0530

    initials

---
+++ EcomGrove/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns = [
+    path('jet/', include('jet.urls')),
+    path('jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
+    path('admin/', admin.site.urls),
+    path('', include('home.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
